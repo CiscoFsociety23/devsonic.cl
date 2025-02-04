7d4b7834bfdad10cec08fd15d8fe0a5dfe84c374
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-02-04 04:53:54 -0300

    feat: adding not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('@pages/home/home.component').then(comp => comp.HomeComponent) }
+    { path: '', loadComponent: () => import('@pages/home/home.component').then(comp => comp.HomeComponent) },
+    { path:"**", loadComponent: () => import('@pages/not-found/not-found.component').then(comp => comp.NotFoundComponent) }
 ];
