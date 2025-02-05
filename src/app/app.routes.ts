2483f649d4e938f24282bb4383c9ac257f80eb73
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-02-05 02:17:44 -0300

    feat: adding portfolio page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('@pages/home/home.component').then(comp => comp.HomeComponent) },
+    { path: 'portfolio', loadComponent: () => import('@pages/portfolio/portfolio.component').then(comp => comp.PortfolioComponent) },
     { path:"**", loadComponent: () => import('@pages/not-found/not-found.component').then(comp => comp.NotFoundComponent) }
 ];
