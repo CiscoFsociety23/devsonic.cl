2ea964e3714b95b843a89cd5ab9a34b48d9db6b2
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-02-05 21:23:53 -0300

    feat: adding not-found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-not-found',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    ScrollReveal().reveal('.not-found-content img', {
+      duration: 1000,
+      origin: 'top',
+      distance: '100px',
+      easing: 'ease-in-out',
+      delay: 250
+    });
+
+    ScrollReveal().reveal('.not-found-content h1', {
+      duration: 1000,
+      origin: 'bottom',
+      distance: '100px',
+      delay: 250
+    });
+  }
 
 }
