46cd5332a687efa471cda3b45a798d8f5fa3d1b8
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-02-11 20:53:44 -0300

    feat: adding proyects page and environments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ export const routes: Routes = [
     { path: 'portfolio', loadComponent: () => import('@pages/portfolio/portfolio.component').then(comp => comp.PortfolioComponent) },
     { path: 'music', loadComponent: () => import('@pages/music/music.component').then(comp => comp.MusicComponent) },
     { path: 'photography', loadComponent: () => import('@pages/photography/photography.component').then(comp => comp.PhotographyComponent) },
+    { path: 'more-proyects', loadComponent: () => import('@pages/proyects/proyects.component').then(comp => comp.ProyectsComponent) },
     { path:"**", loadComponent: () => import('@pages/not-found/not-found.component').then(comp => comp.NotFoundComponent) }
 ];
